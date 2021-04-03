faedbf276cf6d87ab601ae1c1bbbe1bc0847d699
Anisha <anisha.0612@live.com>
2021-04-03 15:44:19 -0400

    Project startup files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FormInput from "./components/FormInput/FormInput.jsx";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <FormInput />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/FormInput/FormInput.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const FormInput = () => {
+  return (
+    <div>
+      <Form></Form>
+    </div>
+  );
+};
+
+export default FormInput;
