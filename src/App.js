b7b4c85ee8ac1fdea0537ce8a7f07c41e650b5fc
Anisha <anisha.0612@live.com>
2021-04-04 18:39:24 -0400

    add calculations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router";
-import FormInput from "./components/FormInput/FormInput.jsx";
+import FormInput from "./pages/FormInput/FormInput.jsx";
+import BillSummary from "./pages/BillSummary/BillSummary.jsx";
 import "./App.css";
 
 const App = () => {
+  const [form, setForm] = useState({
+    miles: 10000,
+    rate: "A",
+    time: "night",
+  });
+
+  // Handling Form input change
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className='App'>
       <Switch>
-        <Route exact path='/' component={FormInput} />
+        <Route
+          exact
+          path='/'
+          component={() => (
+            <FormInput handleChange={handleChange} form={form} />
+          )}
+        />
+        <Route exact path='/summary' component={BillSummary} />
       </Switch>
     </div>
   );
---
+++ src/pages/BillSummary/BillSummary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BillSummary = () => {
+  return <div>Summary</div>;
+};
+
+export default BillSummary;
